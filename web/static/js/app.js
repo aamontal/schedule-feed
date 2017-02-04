034eaef7a2d0b0ad7e7fa9e5261c927b97a28a43
Andrew Montalto <andrew@boundless.com>
2017-02-04 17:40:31 -0500

    fix the clear previousSeach logic

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -55,7 +55,7 @@ class ScheduleFeedDepartures extends React.Component {
   formatMap(column, value) {
     const action = {
       ScheduledTime:  this.formatTime,
-      EstDeparture:  this.formatTime,
+      EstDeparture:   this.formatTime,
       TimeStamp:      this.formatDate,
       Status:         this.foramtStatus,
       Track:          (track) => { return Boolean(track) ? track : "TBD" }
@@ -80,7 +80,7 @@ class ScheduleFeedDepartures extends React.Component {
   }
 
   componentDidMount() {
-    this.fetchSchedule()
+    this.executeSearch(this.fetchSchedule.bind(this))
     this.togglePolling(this.state.poll)
   }
 
@@ -92,46 +92,49 @@ class ScheduleFeedDepartures extends React.Component {
         return true
       }
     })
-
     const revSchedule = filteredSchedule.map(this.addEstDeparture.bind(this))
     this.setState({schedule: revSchedule})
   }
 
+  executeSearch(search) {
+    this.previousSearch && this.previousSearch.abort()
+    clearTimeout(this.previousTimer)
+
+    this.previousTimer = setTimeout( () =>
+      this.previousSearch = search(),
+      500
+    )
+  }
+
   fetchSchedule() {
-    $.getJSON(this.props.endpoint)
+    console.log("FETCHING")
+    return $.getJSON(this.props.endpoint)
       .done(this.setSchedule.bind(this))
       .fail(() => {
         alert("Error fetching departures, please try again")
       })
   }
 
-  handleKeyPress(e) {
-    let value = e.target.value
-
-    const filterSearch = (filterValue) => {
-      this.setState({filter: filterValue.toLowerCase()})
-      this.fetchSchedule()
-    }
-
-    //prevent racecondition in case server is slow
-    this.previousSearch && this.previousSearch.abort()
-    clearTimeout(this.previousTimer)
-
-    //give user sometime to type
-    this.previousTimer = setTimeout( () =>
-      filterSearch(value),
-      500
-    )
-  }
 
   togglePolling(check) {
-     if(check){
-      let interval = setInterval(this.fetchSchedule.bind(this), parseInt(this.props.intervalTime))
+    if(check){
+      const search = () => { this.executeSearch(this.fetchSchedule.bind(this)) }
+      const interval = setInterval(search, parseInt(this.props.intervalTime))
       this.setState({interval: interval, poll: true})
     } else {
       this.setState({poll: false})
       clearInterval(this.state.interval)
     }
+  }
+
+  handleKeyPress(e) {
+    const value = e.target.value
+    const filterSearch = (filterValue) => {
+      this.setState({filter: filterValue.toLowerCase()})
+      return this.fetchSchedule()
+    }
+
+    this.executeSearch(() => filterSearch(value))
   }
 
   handlePollChange(e) {
@@ -140,12 +143,12 @@ class ScheduleFeedDepartures extends React.Component {
 
   handleButtonClick(e) {
     e.preventDefault()
-    this.fetchSchedule()
+    this.executeSearch(this.fetchSchedule.bind(this))
   }
 
   now() {
     const today = new Date()
-    const day = today.toLocaleString('en-us', {  weekday: 'long' })
+    const day = today.toLocaleString('en-us', { weekday: 'long' })
     const date = today.toLocaleDateString('en-us')
     return `${day} ${date}`
   }
@@ -208,7 +211,7 @@ class ScheduleFeedDepartures extends React.Component {
 ReactDOM.render(
   <ScheduleFeedDepartures
     intervalTime={ 5000 }
-    polling={false}
-    endpoint={window.APP_API_DEPARTURES}
+    polling={ false }
+    endpoint={ window.APP_API_DEPARTURES }
   />, document.getElementById("departureApp")
 )
